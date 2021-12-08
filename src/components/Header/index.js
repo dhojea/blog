0223cc1677458de3b728149cd454d5a4a16e83e1
Diego <dh.ojea>
2021-12-08 12:44:59 +0100

    Styling the firts version of  menu.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,31 @@
+import logosvg from '../../svg/logo.svg';
 
 function Header() {
 
+    const descLogo = "This is logo of Blog."
 
     return (
-        <h1> Header </h1>
+       <>
+        <header className="flex-space-between">
+            <div className="logo">
+                <img src={logosvg} alt={descLogo}/>
+            </div>
+
+            <div className="search">
+                <input  className="input-search" type="text" name="search" placeholder="Search a article"></input>
+            </div>
+
+            <div className="menu">
+                <ul>
+                    <li><a href="#" className="nav-link"> Categories</a></li>
+                    <li><a href="#" className="nav-link"> About</a></li>
+                    <li><a href="#" className="nav-link"> Contact</a></li>
+
+                </ul>
+            </div>
+
+        </header>
+       </>
     );
 }
 
